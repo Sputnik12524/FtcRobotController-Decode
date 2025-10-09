e52ed1ab0d98b7a075215b9d40ce310e8d05af69
VladimirAkhramovich12524 <v.akhramovich@lab244.ru>
2025-10-09 19:09:29 +0300

    Update Shooter.java
    Добавил сырой код

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/Shooter.java
@@ -1,4 +1,46 @@
 package org.firstinspires.ftc.teamcode.modules;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
 public class Shooter {
+
+    private DcMotor motor;
+    private Servo cover;
+
+    public static double POWER;
+    public static double SHORT_THROW;
+    public static double LONG_THROW;
+    private boolean isItShortThrow = false;
+
+    public Shooter (LinearOpMode opMode) {
+        motor = opMode.hardwareMap.get(DcMotor.class, "shooter");
+        cover = opMode.hardwareMap.servo.get("cover");
+    }
+
+    public void shoot() {
+        motor.setPower(POWER);
+    }
+    public void setLongThrow() {
+        cover.setPosition(LONG_THROW);
+        isItShortThrow = false;
+    }
+    public void setShortThrow() {
+        cover.setPosition(SHORT_THROW);
+        isItShortThrow = true;
+    }
+    public void switchTrowMode() {
+        if (isItShortThrow) {
+            cover.setPosition(LONG_THROW);
+            isItShortThrow = false;
+        } else {
+            cover.setPosition(SHORT_THROW);
+            isItShortThrow = true;
+        }
+    }
+
+
+
+
 }
