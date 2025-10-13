b0d5d7bbce101b293b7999b247b71ff2fd243180
DianaLebedeva12524 <d.lebedeva@lab244.ru>
2025-10-13 17:36:50 +0300

    da

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/Parking.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/Parking.java
@@ -1,4 +1,12 @@
 package org.firstinspires.ftc.teamcode.modules;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 public class Parking {
+    private DcMotor upMotor;
+
+    public Parking (LinearOpMode linearOpMode){
+        this.upMotor = linearOpMode.hardwareMap.get(DcMotor.class, "upMotor" );
+    }
 }
