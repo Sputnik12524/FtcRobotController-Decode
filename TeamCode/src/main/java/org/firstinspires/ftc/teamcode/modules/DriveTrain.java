dc2d3dd600746cbdfbb72790f7d51fc052fa9e8f
DianaLebedeva12524 <d.lebedeva@lab244.ru>
2025-10-13 18:33:55 +0300

    start parking

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/DriveTrain.java
@@ -1,4 +1,5 @@
 package org.firstinspires.ftc.teamcode.modules;
 
 public class DriveTrain {
+
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/Parking.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/Parking.java
@@ -2,11 +2,28 @@ package org.firstinspires.ftc.teamcode.modules;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class Parking {
     private DcMotor upMotor;
+    private Servo servo;
 
-    public Parking (LinearOpMode linearOpMode){
-        this.upMotor = linearOpMode.hardwareMap.get(DcMotor.class, "upMotor" );
+    public static double POWER = 1;
+    public static double UP = 1000;
+    public static double POS = 0;
+
+
+
+    public Parking(LinearOpMode linearOpMode) {
+        this.upMotor = linearOpMode.hardwareMap.get(DcMotor.class, "upMotor");
+        upMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        upMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//??  Не уверена сюда ли направление
+        upMotor.setDirection(DcMotor.Direction.FORWARD);
+    }
+
+    public void setPower() {
+        upMotor.setPower(POWER);
     }
 }
