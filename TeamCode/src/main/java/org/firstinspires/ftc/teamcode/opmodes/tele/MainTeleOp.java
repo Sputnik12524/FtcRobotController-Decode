9da1c4b2918d86c868093d4f362f692d18e4724f
Avdyukov Nikita <n.avdyukov@lab244.ru>
2025-10-17 18:23:50 +0300

    Made a class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tele/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tele/MainTeleOp.java
@@ -1,4 +1,26 @@
 package org.firstinspires.ftc.teamcode.opmodes.tele;
 
-public class MainTeleOp {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.teamcode.modules.DriveTrain;
+import org.firstinspires.ftc.teamcode.modules.Intake;
+import org.firstinspires.ftc.teamcode.modules.Parking;
+import org.firstinspires.ftc.teamcode.modules.Shooter;
+import org.firstinspires.ftc.teamcode.modules.Sorting;
+@TeleOp(name = "MainTeleop", group = "Robot")
+
+public class MainTeleOp extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+        DriveTrain dt = new DriveTrain();
+        Intake in = new Intake();
+        Shooter st = new Shooter();
+        Parking pk = new Parking();
+        Sorting sr = new Sorting();
+
+        waitForStart();
+        while (opModeIsActive()) {
+        }
+    }
 }
