02bf97bb51ee61a4f348d70135caa2211a157c73
VladimirAkhramovich12524 <v.akhramovich@lab244.ru>
2025-10-20 19:14:01 +0300

    Добавил значения для переменных, отформатировал класс и добавил тест сырой

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/Shooter.java
@@ -9,12 +9,12 @@ public class Shooter {
     private DcMotor motor;
     private Servo cover;
 
-    public static double POWER;
-    public static double SHORT_THROW;
-    public static double LONG_THROW;
+    public static double POWER = 1;
+    public static double SHORT_THROW = 0;
+    public static double LONG_THROW = 1;
     private boolean isItShortThrow = false;
 
-    public Shooter (LinearOpMode opMode) {
+    public Shooter(LinearOpMode opMode) {
         motor = opMode.hardwareMap.get(DcMotor.class, "shooter");
         cover = opMode.hardwareMap.servo.get("cover");
     }
@@ -22,14 +22,17 @@ public class Shooter {
     public void shoot() {
         motor.setPower(POWER);
     }
+
     public void setLongThrow() {
         cover.setPosition(LONG_THROW);
         isItShortThrow = false;
     }
+
     public void setShortThrow() {
         cover.setPosition(SHORT_THROW);
         isItShortThrow = true;
     }
+
     public void switchTrowMode() {
         if (isItShortThrow) {
             cover.setPosition(LONG_THROW);
@@ -39,8 +42,6 @@ public class Shooter {
             isItShortThrow = true;
         }
     }
-
-
 
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/test/ShooterAndSortingTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/test/ShooterAndSortingTest.java
@@ -1,4 +1,32 @@
 package org.firstinspires.ftc.teamcode.opmodes.test;
 
-public class ShooterAndSortingTest {
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp (group = "Robot")
+public class ShooterAndSortingTest extends LinearOpMode {
+    DcMotor motor;
+    double power = 1;
+
+    public void runOpMode() {
+        motor = hardwareMap.get(DcMotor.class, "motor");
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            if(gamepad1.a) {
+                motor.setPower(power);
+            } else if (gamepad1.b) {
+                motor.setPower(power);
+            } else {
+                motor.setPower(0);
+            }
+
+        }
+
+    }
 }
+
