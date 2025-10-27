902a14ba74b88638f4ca560fdf8d2ef4328f119b
DS12524 <d.smirnova@lab244.ru>
2025-10-27 21:00:20 +0300

    added classes for auto goal red&blue

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -18,11 +18,17 @@ public class MeepMeepTesting {
         MeepMeep meepMeep = new MeepMeep(500, 500);
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(-47, -45, Math.toRadians(45)))
-                        .waitSeconds(1)
-                        .lineToLinearHeading(new Pose2d(-29, -25, Math.toRadians(-90)))
-                        .strafeTo(new Vector2d(-11, -27))
-                        .forward(5)
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(-49, 49, Math.toRadians(-45)))
+                        .waitSeconds(5)
+                        .lineTo(new Vector2d(-29, 25))
+                        .waitSeconds(5)
+                        .lineToLinearHeading(new Pose2d(-11, 27, Math.toRadians(90)))
+                        .waitSeconds(5)
+                        .forward(20)
+                        .lineToLinearHeading(new Pose2d(-29,25, Math.toRadians(-45)))
+                        .waitSeconds(5)
+                        .lineToLinearHeading(new Pose2d(-50,25, Math.toRadians(0)))
+                        .waitSeconds(10)
                         .build());
 
         Image img = null;
