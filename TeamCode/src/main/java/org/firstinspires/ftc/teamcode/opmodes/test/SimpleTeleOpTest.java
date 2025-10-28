3483f1a381e0e280b33299f955718f2a5354800f
Dashaa <d.smirnova@lab244.ru>
2025-10-28 19:00:01 +0300

    added simple teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/test/SimpleTeleOpTest.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.opmodes.test;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.modules.DriveTrain;
+
+@TeleOp(name="Simple TeleOp")
+public class SimpleTeleOpTest extends LinearOpMode {
+    DriveTrain dt = new DriveTrain(this);
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        waitForStart();
+
+        double main = gamepad1.left_stick_y;
+        double side = gamepad1.left_stick_x;
+        double rotate = gamepad1.left_trigger - gamepad1.right_trigger;
+
+        if(isStopRequested()) return;
+
+        dt.setMotorPower(main,side,rotate);
+
+    }
+}
