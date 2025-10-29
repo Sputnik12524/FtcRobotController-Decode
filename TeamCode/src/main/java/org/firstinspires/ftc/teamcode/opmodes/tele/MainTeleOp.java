cfd33939679e8555824abd8709d86138c98640be
Dashaa <d.smirnova@lab244.ru>
2025-10-29 16:50:10 +0300

    edited basic values in three-wheel localizer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tele/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tele/MainTeleOp.java
@@ -1,4 +1,21 @@
 package org.firstinspires.ftc.teamcode.opmodes.tele;
 
-public class MainTeleOp {
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.modules.drivetrainrr.DriveTrainMecanum;
+import org.firstinspires.ftc.teamcode.modules.drivetrainrr.ThreeWheelLocalizer;
+
+@TeleOp(name="TeleOp RoadRunner", group="Robot")
+public class MainTeleOp extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        DriveTrainMecanum dt = new DriveTrainMecanum(hardwareMap);
+        ThreeWheelLocalizer loc = new ThreeWheelLocalizer(hardwareMap);
+
+        waitForStart();
+
+    }
 }
