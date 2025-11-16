6d6c1cb97f2dcf5a55d3756809ebd1c7a08dfa0f
Dashaa <d.smirnova@lab244.ru>
2025-11-16 12:55:54 +0300

    working teleoprr

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tele/TeleOpRoadRunner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tele/TeleOpRoadRunner.java
@@ -31,12 +31,16 @@ public class TeleOpRoadRunner extends LinearOpMode {
 
     /// Sorting
     boolean isOpen = false;
-    boolean stateY = false;
+    boolean stateY2 = false;
+    boolean stateY1 = false;
     boolean stateB2 = false;
-    boolean stateDown = false;
-
-    boolean stateRight = false;
-    boolean stateUp = false;
+    boolean stateDown1 = false;
+    boolean stateRight1 = false;
+    boolean stateUp1 = false;
+    boolean stateLeft1 = false;
+    boolean stateDown2 = false;
+    boolean stateRight2 = false;
+    boolean stateUp2 = false;
 
     public static double POWER_LOWEST = 0.76;
     public static double POWER_MIDDLE = 0.8;
@@ -48,7 +52,7 @@ public class TeleOpRoadRunner extends LinearOpMode {
 
         ll = new Limelight(this);
         timer = new ElapsedTime();
-        sorting = new Sorting(this, ll);
+        sorting = new Sorting(this);
         st = new Shooter(this);
         in = new Intake(this);
         DriveTrainMecanum dt = new DriveTrainMecanum(hardwareMap);
@@ -65,20 +69,33 @@ public class TeleOpRoadRunner extends LinearOpMode {
 
             // INTAKE and SORTING
             if (gamepad1.a && !isRotateIn && !stateA1) {
-                //sorting.intaker.start();
                 sorting.wallForIntaking();
                 in.rotateIn();
                 isRotateIn = true;
                 isRotateOut = false;
             } else if (gamepad1.a && isRotateIn && !stateA1) {
                 in.rotateStop();
-                //sorting.intaker.interrupt();
                 isRotateIn = false;
             }
 
-            // SORTING
-            //  if (gamepad2.b && !stateB2) {
-            // sorting.sortingArtefacts(sorting.artefact_pos(sorting.getColor()), sorting.aprilTagToScan(sorting.scanId));
+             //SORTING
+//            if (gamepad1.dpad_down && !stateDown1) {
+//                sorting.drumMotor.setPower(0.2);
+//                while(sorting.drumMotor.getCurrentPosition() % 120 == 0);
+//                sorting.drumStop();
+//                sorting.regulatorSorting.start();
+//                sorting.sortingArtefacts(sorting.artefact_pos(sorting.getColor()), Sorting.Scan.LEFT);
+//            }
+//            if (gamepad1.dpad_right && !stateRight1) {
+//                sorting.regulatorSorting.start();
+//                sorting.sortingArtefacts(sorting.artefact_pos(sorting.getColor()), Sorting.Scan.BETWEEN);
+//            }
+//            if (gamepad1.dpad_up && !stateUp1) {
+//                sorting.regulatorSorting.start();
+//                sorting.sortingArtefacts(sorting.artefact_pos(sorting.getColor()), Sorting.Scan.RIGHT);
+//            }
+//            if (gamepad1.dpad_left && !stateLeft1) {
+//                sorting.regulatorSorting.interrupt();
 //            }
 
             // SHOOTER and SORTING с дальней
@@ -86,41 +103,33 @@ public class TeleOpRoadRunner extends LinearOpMode {
                 sorting.wallForShooting();
                 st.shootByPower(1);
                 isShooting = true;
-                //sorting.shooter.start();
             } else if (gamepad2.a && isShooting && !stateA2) {
-                //sorting.shooter.interrupt();
                 st.shootStop();
                 isShooting = false;
             }
 
             //стрельба с ближней 0.76
-            if (gamepad2.dpad_down && !isShooting && !stateDown) {
+            if (gamepad2.y && !isShooting && !stateY2) {
                 sorting.wallForShooting();
                 st.shootByPower(POWER_LOWEST);
                 isShooting = true;
-            } else if (gamepad2.dpad_down && isShooting && !stateDown) {
+            } else if (gamepad2.y && isShooting && !stateY2) {
                 st.shootStop();
                 isShooting = false;
             }
 
-            //стрельба с ближней 0.8
-            if (gamepad2.dpad_right && !isShooting && !stateRight) {
+            // увеличение мощности при стрельбе
+            if (gamepad2.dpad_up && isShooting && !stateUp2) {
                 sorting.wallForShooting();
-                st.shootByPower(POWER_MIDDLE);
+                POWER_LOWEST += 0.05;
                 isShooting = true;
-            } else if (gamepad2.dpad_right && isShooting && !stateRight) {
-                st.shootStop();
-                isShooting = false;
             }
 
-            //стрельба с ближней 0.85
-            if (gamepad2.dpad_up && !isShooting && !stateUp) {
+            // уменьшение мощности при стрельбе
+            if (gamepad2.dpad_down && isShooting && !stateDown2) {
                 sorting.wallForShooting();
-                st.shootByPower(POWER_HIGHEST);
+                POWER_LOWEST -= 0.05;
                 isShooting = true;
-            } else if (gamepad2.dpad_up && isShooting && !stateUp) {
-                st.shootStop();
-                isShooting = false;
             }
 
             //IF TROUBLES
@@ -140,10 +149,10 @@ public class TeleOpRoadRunner extends LinearOpMode {
                 sorting.drumTeleStop();
             }
 
-            if (gamepad2.y && !isOpen && !stateY) {
+            if (gamepad2.b && !isOpen && !stateB2) {
                 sorting.horizontalWallOpen();
                 isOpen = true;
-            } else if (gamepad2.y && isOpen && stateY) {
+            } else if (gamepad2.b && isOpen && !stateB2) {
                 sorting.horizontalWallClose();
                 isOpen = false;
             }
@@ -152,10 +161,15 @@ public class TeleOpRoadRunner extends LinearOpMode {
             stateB1 = gamepad1.b;
             stateA2 = gamepad2.a;
             stateB2 = gamepad2.b;
-            stateY = gamepad2.y;
-            stateDown = gamepad2.dpad_down;
-            stateRight = gamepad2.dpad_right;
-            stateUp = gamepad2.dpad_up;
+            stateY2 = gamepad2.y;
+            stateY1 = gamepad1.y;
+            stateDown2 = gamepad2.dpad_down;
+            stateRight2 = gamepad2.dpad_right;
+            stateUp2 = gamepad2.dpad_up;
+            stateDown1= gamepad1.dpad_down;
+            stateRight1 = gamepad1.dpad_right;
+            stateUp1 = gamepad1.dpad_up;
+            stateLeft1 = gamepad1.dpad_left;
         }
     }
 
