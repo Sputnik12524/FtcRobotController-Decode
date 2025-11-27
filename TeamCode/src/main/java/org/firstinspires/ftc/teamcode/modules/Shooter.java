1f4adcf569c977a5cffaa8fa720ec88f097df446
DianaLebedeva12524 <d.lebedeva@lab244.ru>
2025-11-27 19:54:33 +0300

    edited velocity test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/Shooter.java
@@ -2,19 +2,23 @@ package org.firstinspires.ftc.teamcode.modules;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
 @Config
 public class Shooter {
 
     public final DcMotorEx shooter;
+    public final DcMotorEx shooterUp;
     private final Servo cover;
+    public static double TURNOVER = 360;
 
     public static double POWER = 1; // было 0.83
+    public static double POWER2 = 1;
     public static double SHORT_THROW = 0.1;
     public static double LONG_THROW = 0.2;
     public static double target;
@@ -25,6 +29,8 @@ public class Shooter {
 
     public Shooter(LinearOpMode opMode) {
         shooter = opMode.hardwareMap.get(DcMotorEx.class, "shooter");
+        shooterUp = opMode.hardwareMap.get(DcMotorEx.class, "shooter2");
+
         cover = opMode.hardwareMap.get(Servo.class, "cover");
 
         shooter.setDirection(DcMotorSimple.Direction.FORWARD);
@@ -36,14 +42,17 @@ public class Shooter {
 
     public void shoot(){
         shooter.setPower(POWER);
+        shooterUp.setPower(POWER);
     }
 
     public void shootStop() {
         shooter.setPower(0);
+        shooterUp.setPower(0);
     }
 
-    public void setVelocity () {
-        shooter.setVelocity(5000);
+    public void setVelocity (double TURNOVER) {
+        shooter.setVelocity(TURNOVER, AngleUnit.DEGREES);
+        shooterUp.setVelocity(TURNOVER, AngleUnit.DEGREES);
     }
 
     public void setLongThrow() {
@@ -68,11 +77,6 @@ public class Shooter {
 
     public void setPower(double power){
         POWER = power;
-    }
-
-    public void shooterRegulator(){
-        target = POWER - shooter.getCurrentPosition();
-        shooter.setPower(error * target);
     }
 
     public class ContinuousShooter extends Thread {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/test/SetVelocityTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/test/SetVelocityTest.java
@@ -2,30 +2,40 @@ package org.firstinspires.ftc.teamcode.opmodes.test;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
 
-import org.firstinspires.ftc.teamcode.modules.Intake;
-import org.firstinspires.ftc.teamcode.modules.Limelight;
 import org.firstinspires.ftc.teamcode.modules.Shooter;
-import org.firstinspires.ftc.teamcode.modules.Sorting;
 
 @TeleOp(name = "setVelocityTest", group = "Test")
 public class SetVelocityTest extends LinearOpMode {
+    public static double SPEED = 360;
+    public static double SPEED_LOW = 1;
+    public static double SPEED_HIGH = 100;
+    public static double SPEED_MIDDLE = 20;
     Shooter sh;
+
+    boolean isShooting = false;
+    boolean stateA1 = false;
 
     @Override
     public void runOpMode() {
+
         sh = new Shooter(this);
 
         waitForStart();
         while (opModeIsActive()) {
 
-            if (gamepad1.a){
-                sh.setVelocity();
-            } else {
-                sh.shootStop();
+            if (gamepad1.a && !isShooting && !stateA1) {
+                sh.setVelocity(SPEED_LOW);
+                isShooting = true;
+            } else if (gamepad1.a && isShooting && !stateA1) {
+                sh.setVelocity(0);
+                isShooting = false;
             }
+            stateA1 = gamepad1.a;
+
             telemetry.addData("Shooter Velocity", sh.shooter.getVelocity());
+            telemetry.addData("ShooterUP Velocity", sh.shooterUp.getVelocity());
+            telemetry.update();
         }
     }
 }
