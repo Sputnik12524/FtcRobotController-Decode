cdbbaeda81fae5c6286d04972dae9b762061bc84
DianaLebedeva12524 <d.lebedeva@lab244.ru>
2025-12-04 18:09:22 +0300

    parking wit testparking

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/Parking.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/Parking.java
@@ -1,4 +1,35 @@
 package org.firstinspires.ftc.teamcode.modules;
 
-public class Parking{
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Config
+public class Parking {
+    public final DcMotor upMotor;
+
+    public static double POWER = 1; //узнать значение
+    public static int MAX_POSITION = 1000;
+    public static int MIN_POSITION = 0;
+
+    public Parking(LinearOpMode linearOpMode) {
+        this.upMotor = linearOpMode.hardwareMap.get(DcMotor.class, "park");
+        upMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        upMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    public void parkingPower(double POWER) {
+        upMotor.setPower(POWER);
+    }
+    public  void parkingStop(){
+        upMotor.setPower(0);
+    }
+
+    public void getUpOrDown() {
+        if (upMotor.getCurrentPosition() <= MAX_POSITION){
+            upMotor.setTargetPosition(MAX_POSITION);
+        } else if (upMotor.getCurrentPosition() == MAX_POSITION){
+            upMotor.setTargetPosition(MIN_POSITION);
+        }
+    }
 }
