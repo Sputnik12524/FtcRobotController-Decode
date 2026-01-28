0b08c5d4a212a48e987f48c37753bb09cc545b0e
Avdyukov Nikita <n.avdyukov@lab244.ru>
2026-01-28 19:47:54 +0300

    Логгер (без теста), правки teleopV2(без dt)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/Shooter.java
@@ -14,6 +14,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Config
 public class Shooter {
+    //Config
     public final DcMotorEx shooterUpper;
     LinearOpMode opMode;
     public final DcMotorEx shooterLower;
@@ -21,43 +22,35 @@ public class Shooter {
     public final Servo cover;
     private final VoltageSensor batteryVoltageSensor;
     Follower follower;
+
+    //Velocity
     public static PIDFCoefficients MOTOR_VELO_PID_SHOOTERS = new PIDFCoefficients(26, 0, 10, 15);
     private final double TPR = 28;
-    public short artifacts = 0;
-    public short artifactsIn = 0;
-    public short artifactsNow = 0;
-    public int timers;
-    public static double POWER = 1;
     public double velocityTarget = 0;
     public static double VELOCITY_FOR_LONG_THROW = 52.5;
     public static double VELOCITY_FOR_SHORT_THROW = 40;
-    public static double ERROR = 2.5;
+
+    //Cover
     public static double POS_COVER_OPEN = 0.4;
     public static double POS_COVER_CLOSE = 0.9;
+
+    //Adjuster
     public static double POS_SHORT_THROW = 0.05;
     public static double POS_LONG_THROW = 0;
-    public static double TIME_BETWEEN_SHOOT = 275;
-    public static double TIME_AFTER_SHOOT = 300;
+    public static double ADJUSTER_LIMIT = 0.1;
     public static double DELTA_ADJUSTER = 0.01;
+
+    //Calculator
+    public static double TIME_BETWEEN_SHOOT = 275;
+    public short artifacts = 0;
+    public static double TIME_AFTER_SHOOT = 300;
     public static double DETECT_SHOOT = 3.5;
     public static double IS_SPIN_UP = 2.5;
-    public boolean isShooting = false;
     public boolean detected = false;
     public boolean complete = false;
-    boolean isSpinUp = false;
-    int timerses = 0;
+    public boolean completeC = false;
 
-    public static double TIME_GATES_BETWEEN_SHOOT = 1000;
-    public static double TIME_FOR_SET_VELOCITY = 2500;
     private final ElapsedTime timer = new ElapsedTime();
-
-    boolean InZone = true;
-    public static boolean isTunnelOpen;
-
-    enum states {DEFAULT, INIT, SHOOT, UPDATE, RESTART, START}
-
-    states state = states.INIT;
-    private final ElapsedTime timerSh = new ElapsedTime();
     private Pose currentPose;
 
     public Shooter(LinearOpMode opMode) {
@@ -109,15 +102,9 @@ public class Shooter {
         velocityTarget = targetInRPS * TPR;
     }
 
-    public void shootByPower() {
-        shooterUpper.setPower(POWER);
-        shooterLower.setPower(POWER);
-    }
-
     public void shootStop() {
         shooterUpper.setVelocity(0);
         shooterLower.setVelocity(0);
-        isShooting = false;
     }
 
     private void setPIDFCoefficients(DcMotorEx motor, PIDFCoefficients coefficients) {
@@ -136,7 +123,6 @@ public class Shooter {
 
     public void openTunnel() {
         cover.setPosition(POS_COVER_OPEN);
-        isShooting = true;
 
     }
 
@@ -148,10 +134,10 @@ public class Shooter {
         return angleAdjuster.getPosition();
     }
 
-       public boolean ifInLaunchZoneGoal() {
-    currentPose = follower.getPose();
-           return follower.getPose().getY() >= Math.abs(follower.getPose().getX() - 72) + 72;
-     }
+    public boolean ifInLaunchZoneGoal() {
+        currentPose = follower.getPose();
+        return follower.getPose().getY() >= Math.abs(follower.getPose().getX() - 72) + 72;
+    }
 
     public boolean ifInLaunchZoneHuman() {
         currentPose = follower.getPose();
@@ -165,8 +151,7 @@ public class Shooter {
 
     public void setMode(double pos) {
         if (pos < 0) angleAdjuster.setPosition(0);
-        else angleAdjuster.setPosition(pos);
-       // else angleAdjuster.setPosition(Math.max(pos, POS_SHORT_THROW));
+        else angleAdjuster.setPosition(Math.max(pos, ADJUSTER_LIMIT));
     }
 
     public void threeArtefactsShooting() {
@@ -176,7 +161,7 @@ public class Shooter {
                 timer.reset();
                 while (timer.milliseconds() < TIME_BETWEEN_SHOOT) {
                 }
-               // angleAdjuster.setPosition(angleAdjuster.getPosition() + DELTA_ADJASTER);
+                // angleAdjuster.setPosition(angleAdjuster.getPosition() + DELTA_ADJASTER);
                 setMode(angleAdjuster.getPosition() + DELTA_ADJUSTER);
             }
             complete = false;
@@ -184,6 +169,8 @@ public class Shooter {
             while (timer.milliseconds() < TIME_AFTER_SHOOT) {
             }
             setShortThrowMode();
+            completeC = true;
+
         }
     }
 
@@ -224,20 +211,6 @@ public class Shooter {
 
     public double getVelocityTPS() {
         return shooterUpper.getVelocity();
-    }
-
-    public void sleeping(long ms) {
-        try {
-            Thread.sleep(ms);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-
-    public void transit(states state) {
-        timer.reset();
-        this.state = state;
     }
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Logger.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Logger.java
@@ -2,18 +2,25 @@ package org.firstinspires.ftc.teamcode.util;
 
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class Logger {
     String directoryPath = "/sdcard/FIRST/";
-    FileWriter writer;
     BufferedReader reader;
+    BufferedWriter writer;
+    String fileName;
+    String line;
+    Alliance al;
+    double x;
+    double y;
+
     public Logger(String fileName) {
         try {
-            this.writer = new FileWriter(this.directoryPath + fileName + ".csv", false);
-            this.reader = new BufferedReader(new FileReader(this.directoryPath + fileName + ".csv"));
+            this.fileName = fileName;
+            this.writer = new BufferedWriter(new FileWriter(this.directoryPath + fileName + ".csv", false));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -29,30 +36,38 @@ public class Logger {
 
     public void writePose(Alliance alliance, double x, double y) {
         try {
-            switch (alliance){
-                case NONE:
-                    writer.write("none");
+            switch (alliance) {
                 case RED:
-                    writer.write("Red" + "," + x + "," + y + "\n");
+                    writer.write("Red" + "," + x + "," + y);
+                    break;
                 case BLUE:
-                    writer.write("blue" + "," + x + "," + y + "\n");
+                    writer.write("Blue" + "," + x + "," + y);
+                    break;
                 default:
                     writer.write("none");
             }
-
         } catch (IOException exe) {
             exe.printStackTrace();
         }
     }
 
-//    public String getAlliance(){
-//        try{
-//            return reader.
-//        }catch (IOException exe){
-//            exe.printStackTrace();
-//        }
-//    }
-
+    public void getAll() {
+        try {
+            this.reader = new BufferedReader(new FileReader(this.directoryPath + fileName + ".csv"));
+            line = reader.readLine();
+            if (line != null) {
+                String a[] = line.split(",");
+                if (a[0].equals("Blue")) al = Alliance.BLUE;
+                else if (a[0].equals("Red")) al = Alliance.RED;
+                else al = Alliance.NONE;
+                if (a[1] != null) x = Double.parseDouble(a[1]);
+                if (a[2] != null) y = Double.parseDouble(a[2]);
+            }
+            reader.close();
+        } catch (IOException exe) {
+            exe.printStackTrace();
+        }
+    }
 
     public void addHeader(String header) {
         try {
