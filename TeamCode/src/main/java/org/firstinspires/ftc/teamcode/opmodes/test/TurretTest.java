8971a656160369e64c8dc5f94a31430a4ec771b7
VladimirAkhramovich12524 <v.akhramovich@lab244.ru>
2026-02-21 07:39:25 +0300

    турель косит

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/test/TurretTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/test/TurretTest.java
@@ -7,6 +7,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.modules.Shooter;
 import org.firstinspires.ftc.teamcode.modules.Turret;
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 import org.firstinspires.ftc.teamcode.util.Alliance;
@@ -19,12 +20,13 @@ public class TurretTest extends LinearOpMode {
     Follower follower;
     AutoSniper as;
     Turret tt;
+    Shooter sh;
 
 
     @Override
     public void runOpMode() {
         tt = new Turret(this);
-        as = new AutoSniper(tt);
+        as = new AutoSniper(tt, sh);
         follower = Constants.createFollower(hardwareMap);
         follower.setStartingPose(new Pose(72,72, 0));
         follower.update();
@@ -38,8 +40,11 @@ public class TurretTest extends LinearOpMode {
         while (opModeIsActive()) {
             follower.update();
 
-            as.continuousTurnTurretToGate( follower.getPose().getX(), follower.getPose().getY() ,follower.getHeading());
+            as.continuousTurnTurretToGate( follower.getPose().getX(), follower.getPose().getY() ,Math.toDegrees(follower.getHeading()));
             telemetry.addData("target", tt.target);
+            telemetry.addData("error", tt.error);
+            telemetry.addData("target FROM AutoSniper", as.target);
+            telemetry.addData("angleOfTurret (отн. поля)", as.angleOfTurret);
             telemetry.addData("x:", follower.getPose().getX());
             telemetry.addData("y:", follower.getPose().getY());
             telemetry.addData("head", follower.getPose().getHeading());
