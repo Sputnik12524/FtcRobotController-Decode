0356417c4422b1463ef8784bad8704baf1345424
Dashaa <d.smirnova@lab244.ru>
2026-05-07 17:29:30 +0300

    changed the light test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/test/LightTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/test/LightTest.java
@@ -5,30 +5,29 @@ import com.acmerobotics.dashboard.config.Config;
 import com.pedropathing.follower.Follower;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
 
 import org.firstinspires.ftc.teamcode.modules.Shooter;
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 
 @Config
-@TeleOp(name = "Light", group = "test")
+@TeleOp
 public class LightTest extends LinearOpMode {
     @Override
     public void runOpMode() {
-        Follower fl = Constants.createFollower(hardwareMap);
-        Shooter sh = new Shooter(this, fl);
+        DigitalChannel led = hardwareMap.get(DigitalChannel.class,"LED1");
+        led.setMode(DigitalChannel.Mode.OUTPUT);
         waitForStart();
         while (opModeIsActive()) {
 
             if (gamepad1.a) {
-                sh.light1.setState(true);
-                telemetry.addData("State of sensor", sh.light1.getState());
-            } else if (gamepad1.b) {
-                sh.light2.setState(true);
-                telemetry.addData("Light 2 state", sh.light1.getState());
+                led.setState(true);
+                telemetry.addData("State of sensor", led.getState());
             } else {
-                sh.light1.setState(false);
-                sh.light2.setState(false);
+                led.setState(false);
+                telemetry.addData("State of sensor", led.getState());
             }
+            telemetry.addData("Mode of sensor", led.getMode());
             telemetry.update();
         }
     }
